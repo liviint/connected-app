9ad9b6fa896cf7f88144cb17efe26dd76423eaf7
kevinmm001 <kevinm@teknobuilt.com>
2025-12-10 16:21:17 +0300

    added the habits icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,6 +34,14 @@ export default function TabLayout() {
       />
 
       <Tabs.Screen
+        name="habits/index"
+        options={{
+          title: 'Habits',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="checkmark.seal.fill" color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
         name="profile/index"
         options={{
           title: 'Profile',
