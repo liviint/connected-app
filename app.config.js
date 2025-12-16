06b2ffa9653ce67d1da1001a017bc5c2b771e784
kevinmm001 <kevinm@teknobuilt.com>
2025-12-16 10:45:39 +0300

    fixing deep-linking issues

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.4",
+    version: "1.0.5",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
