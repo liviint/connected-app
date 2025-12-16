e50d73b9dbd32ff642a1136df0fff85866c13f37
kevinmm001 <kevinm@teknobuilt.com>
2025-12-16 14:40:49 +0300

    fixed some redirection issues

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.6",
+    version: "1.0.7",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
