a120dc43d49a28566541447dedd9213accdcf005
kevinmm001 <kevinm@teknobuilt.com>
2025-12-23 06:12:56 +0300

    added the logout url

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.10",
+    version: "1.0.11",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
