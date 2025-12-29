758b5f7c7ef7c35f8a00856a7f81d27f00e8e45e
kevinmm001 <kevinm@teknobuilt.com>
2025-12-29 10:10:25 +0300

    journals sync unctionality

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.12",
+    version: "1.0.13",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
