45afb023bdc50fe3b06385356eaa8cccfc04f3a0
kevinmm001 <kevinm@teknobuilt.com>
2026-01-04 09:06:29 +0300

    Fixed some issues with syncing

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.15",
+    version: "1.0.16",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
