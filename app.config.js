8254b18ce588d2ab5ee23b7c931b4f0cc18f4287
kevinmm001 <kevinm@teknobuilt.com>
2026-01-05 12:20:14 +0300

    installed dependencies

--- app.config.js
+++ app.config.js
@@ -62,10 +62,11 @@ export default {
     plugins: [
       "expo-router",
       "expo-sqlite",
+      "@react-native-community/datetimepicker",
       [
         "expo-splash-screen",
         {
-          image: "./assets/images/splash-icon.png",
+          image: "./assets/images/icon.png",
           imageWidth: 200,
           resizeMode: "contain",
           backgroundColor: "#ffffff",
