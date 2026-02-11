0a60f08048856b4769cc9482f914338c1ad41123
kevinmm001 <kevinm@teknobuilt.com>
2026-02-11 05:42:48 +0300

    version

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.23",
+    version: "1.0.24",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
