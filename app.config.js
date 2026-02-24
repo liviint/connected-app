68685ed8b7f13594e438d8665f5c28c879bfcc56
kevinmm001 <kevinm@teknobuilt.com>
2026-02-24 09:55:45 +0300

    The app is crashing when there are old journals earlier than 7 days, or this month

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.25",
+    version: "1.0.26",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
