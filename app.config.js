9d52399b68574b406e5d78755b313aa065f0df5b
kevinmm001 <kevinm@teknobuilt.com>
2026-02-26 16:32:10 +0300

    added firebase analytics

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.27",
+    version: "1.0.28",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
