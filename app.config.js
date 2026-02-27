cb9b34ce7a29f3fb6b1579c30b8e1f11fc09b0ff
kevinmm001 <kevinm@teknobuilt.com>
2026-02-27 06:55:26 +0300

    added habits deletion syncing

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.28",
+    version: "1.0.29",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
