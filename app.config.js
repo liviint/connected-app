949f2510b3c682b3a5410e159320c97e96b7a486
kevinmm001 <kevinm@teknobuilt.com>
2026-03-20 08:02:02 +0300

    adding donate page

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.32",
+    version: "1.0.33",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
