6513d6fc681222b5f760eaab15b8edd0be3c6007
kevinmm001 <kevinm@teknobuilt.com>
2026-03-24 14:58:56 +0300

    google backup

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.35",
+    version: "1.0.36",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
