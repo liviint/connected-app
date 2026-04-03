893b385724b1b39fbad880b45746875f2e2f6cbc
kevinmm001 <kevinm@teknobuilt.com>
2026-04-03 09:05:39 +0300

    improving all the habits stats page

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     scheme: "zeniahub",
     name: "ZeniaHub",
     slug: "zeniahub",
-    version: "1.0.38",
+    version: "1.0.39",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     userInterfaceStyle: "automatic",
